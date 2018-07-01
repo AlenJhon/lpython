870cf74316b81090686f583bd74a12a4de4924d3
AlenJhon <597815921@qq.com>
2018-07-01 20:07:57 +0800

    add test

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
+#coding:utf-8
+# 对python中yield探索
 
+def gen(n):
+    for i in n:
+        yield i * 2
 
 def main():
     print 'Hello World!'
+    # tmp = gen(2)
+    for x in gen([1,2]):
+        print x
+    
+
+def add(x,y):
+    return x+y
 
 if __name__ == '__main__':
     main()
